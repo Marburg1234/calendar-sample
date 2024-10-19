2c3f9567011be06d3824f2bd32ac9b1b3d8123a8
Marburg1234 <172342315+Marburg1234@users.noreply.github.com>
2024-10-19 13:09:48 +0000

    開発スキルアップ 完成

---
+++ app/javascript/packs/calendar.js
@@ -0,0 +1,23 @@
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import listPlugin from "@fullcalendar/list"; //この行を追加
+
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
+  var calendar = new Calendar(calendarEl, {
+    plugins: [ dayGridPlugin, listPlugin ],
+    initialView: 'dayGridMonth',
+    locale: "jp", //この行を追加 日本語に変換する
+    events: '/events', // eventData変数を使用してイベントデータを渡す
+    //レスポンシブ処理を追加
+    windowResize: function () {
+        if (window.innerWidth < 991.98) {
+            calendar.changeView('listMonth');
+        } else {
+            calendar.changeView('dayGridMonth');
+        }
+    },
+  });
+
+  calendar.render();
+});
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  root to: "events#index"
+  get '/events', to: 'events#index', defaults: { format: 'json' } #この行を追記
+
+end
